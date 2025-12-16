2729ffff0175bc50fe74db715110702a4d7b90cb
Phạm Công Chiến <pcchien250904@gmail.com>
2025-12-17 00:26:46 +0700

    add api update account

--- src/modules/account/dtos/update-account.dto.ts
+++ src/modules/account/dtos/update-account.dto.ts
@@ -1,24 +1,46 @@
-import { IsOptional, IsEmail, Length } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional, IsEmail, Length, IsNotEmpty, IsString, IsNumber } from 'class-validator';
 
 export class UpdateAccountDto {
+  @ApiProperty()
+  @IsNotEmpty({ message: 'ID cannot be null' })
+  @IsString()
+  id!: string;
+
+  @ApiPropertyOptional()
   @IsOptional()
   @Length(3, 255)
   username?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsEmail()
   email?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @Length(6, 255)
   password?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   fullName?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   phone?: string;
 
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber()
+  status?: number;
+
+  @ApiPropertyOptional()
   @IsOptional()
   avatarPath?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString({ message: 'groupId must be a string' })
+  groupId?: string;
 }
